fa58848bf934d1dd7ce7583eb4cf190416e635b4
puranchand <pchand757@gmail.com>
2020-07-18 00:26:27 +0530

    implemented delete functionality and fixes bugs

--- index.js
+++ index.js
@@ -3,19 +3,36 @@ var itemList = [];
 var newArrayList = []
 var item = "";
 
-
-
 function myOnChange(val) {
     textVaule = val;
 }
 
+function deleteItem (index) {
+    if(index !== "" && index !== undefined && index !== -1){
+        newArrayList.splice(index, 1);
+        var text = "";
+        
+        for (i = 0; i < newArrayList.length; i++) {
+            text += `<div onclick="deleteItem(${i})"> ${newArrayList[i]} </div> ` + "<br>";
+        }
+        document.getElementById("myText").innerHTML = text;
+    }    
+}
+
 function handleAddButton() {
-    itemList.push(textVaule);
-    newArrayList = itemList;
     document.getElementById('textInput').value = '';
-    var text = "";
-    for (i = 0; i < newArrayList.length; i++) {
-        text += newArrayList[i] + "<br>";
+
+    if(textVaule !== ""){
+        itemList.push(textVaule);
+        newArrayList = itemList;
+       
+        var text = "";
+        for (i = 0; i < newArrayList.length; i++) {
+            textVaule = "";
+            text += `<div onclick="deleteItem(${i})"> ${newArrayList[i]} </div> ` + "<br>";
+        }
+        document.getElementById("myText").innerHTML = text;
+    }else{
+        return null
     }
-    document.getElementById("myText").innerHTML = text;
 }
