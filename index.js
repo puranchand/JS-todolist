9b7dc4ef5edda5d3bdf62825fa36e6cc5d02bf5a
puranchand <pchand757@gmail.com>
2021-05-25 19:32:55 +0530

    add multiple delete functionality

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 var textVaule = "";
 var itemList = [];
-var newArrayList = []
+var newArrayList = [];
 var item = "";
+var removeItemList = [];
 
 function myOnChange(val) {
     textVaule = val;
+}
+
+function selectItem (index) {
+    if(removeItemList.includes(index)){
+        removeItemList.splice(index, 1);
+    }else{
+        removeItemList.push(index);
+    }
+
+    // if (selectedIndex > -1) {
+    //     removeItemList.splice(selectedIndex, 1);
+    // }else{
+    //     removeItemList.push(index);
+    // }
+
+    console.log(removeItemList, "removeItemList")
 }
 
 function deleteItem (index) {
@@ -13,26 +30,38 @@ function deleteItem (index) {
         var text = "";
         
         for (i = 0; i < newArrayList.length; i++) {
-            text += `<div onclick="deleteItem(${i})"> ${newArrayList[i]} </div> ` + "<br>";
+            text += `<div><button class="check" onclick="selectItem(${i})">check</button>${newArrayList[i]} <span class="delete-icon" onclick="deleteItem(${i})">X</span></div>` + "<br>";
         }
         document.getElementById("myText").innerHTML = text;
     }    
+}
+
+function multipleDelete(){
+    newArrayList = newArrayList.filter((ad, indexId) => removeItemList.every(fd => fd !== indexId));
+    var text = "";
+    for (i = 0; i < newArrayList.length; i++) {
+        text += `<div><button class="check" onclick="selectItem(${i})">check</button>${newArrayList[i]} <span class="delete-icon" onclick="deleteItem(${i})">X</span></div>` + "<br>";
+    }
+
+    document.getElementById("myText").innerHTML = text;
+    setTimeout(function(){ 
+        removeItemList = []
+     }, 1000);
 }
 
 function handleAddButton() {
     document.getElementById('textInput').value = '';
 
     if(textVaule !== ""){
-        itemList.push(textVaule);
-        newArrayList = itemList;
-       
+        newArrayList.push(textVaule)
         var text = "";
         for (i = 0; i < newArrayList.length; i++) {
             textVaule = "";
-            text += `<div onclick="deleteItem(${i})"> ${newArrayList[i]} </div> ` + "<br>";
+            text += `<div><button class="check" onclick="selectItem(${i})">check</button> ${newArrayList[i]} <span class="delete-icon" onclick="deleteItem(${i})">X</span></div> ` + "<br>";
         }
         document.getElementById("myText").innerHTML = text;
     }else{
         return null
     }
 }
+
